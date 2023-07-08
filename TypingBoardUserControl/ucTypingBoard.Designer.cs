74296e1ad9dd3cb6fe57059dea9bbab757f8bdcd
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-07-08 12:46:18 +0100

    Remaking the algorithm with better way (may be)

--- TypingBoardUserControl/ucTypingBoard.Designer.cs
+++ TypingBoardUserControl/ucTypingBoard.Designer.cs
@@ -29,38 +29,98 @@
         private void InitializeComponent()
         {
             this.typing_Board = new ColoredLable.ucColoredLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // typing_Board
             // 
             this.typing_Board.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.typing_Board.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.typing_Board.Dock = System.Windows.Forms.DockStyle.Top;
             this.typing_Board.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold);
             this.typing_Board.Location = new System.Drawing.Point(0, 0);
             this.typing_Board.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.typing_Board.Name = "typing_Board";
-            this.typing_Board.Size = new System.Drawing.Size(968, 214);
+            this.typing_Board.Size = new System.Drawing.Size(968, 115);
             this.typing_Board.TabIndex = 1;
             this.typing_Board.KeyDown += new System.Windows.Forms.KeyEventHandler(this.typing_Board_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 28);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
             // 
             // ucTypingBoard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.typing_Board);
             this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(206)))), ((int)(((byte)(206)))));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ucTypingBoard";
-            this.Size = new System.Drawing.Size(968, 214);
+            this.Size = new System.Drawing.Size(968, 303);
             this.Load += new System.EventHandler(this.ucTypingBoard_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.typing_Board_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private ColoredLable.ucColoredLabel typing_Board;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- TypingBoardUserControl/ucTypingBoard.cs
+++ TypingBoardUserControl/ucTypingBoard.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 
 namespace WindowsFormsControlLibrary1
 {
@@ -20,25 +21,43 @@ namespace WindowsFormsControlLibrary1
             typing_Board.clear();
 
             set("houdaifa bouamine @ gmail . com");
+            
         }
 
         string text = "";
         string input = "";
-
+        string current_word;
 
         Color  colNoType = Color.Gray;
         Color  colCorrectType = Color.FromArgb(220,220,220);
         Color  colWrongType = Color.Red;
 
+        string[] words;
+        Stack<string> writed_words = new Stack<string>();
 
-        int    iRealTextIndex    = 0;
-        int    iWritingTextIndex = 0;
 
+        int iRealTextIndex    = 0;
+        int iWritingTextIndex = 0;
+        int iCurrent_word_index= 0;
+
+        public void add(string new_word)
+        {
+            typing_Board.add_string(new_word + " ", colNoType);
+
+            
+        }
         public void set(string new_text)
         {
             clear();
             text = new_text;
-            typing_Board.add_string(text, colNoType);
+            typing_Board.add_string(new_text, colNoType);
+
+            words = new_text.Split(' ');
+            
+            for(int i = 0; i <  words.Length; i++)
+            {
+                words[i] += ' ';
+            }
         }
         public void clear()
         {
@@ -53,82 +72,187 @@ namespace WindowsFormsControlLibrary1
         }
         private void typing_Board_KeyDown(object sender, KeyEventArgs e)
         {
-            read_char((char)e.KeyValue);
+            read_char(e);
 
             handle_speachal_input(e);
+
+            label1.Text = input;
+
+            label2.Text = current_word;
+            label3.Text = iCurrent_word_index.ToString();
+            label4.Text = words[iCurrent_word_index];
         }
 
-        private void read_char(char ch)
+        private void read_char(KeyEventArgs e)
         {
 
-            if (ch >= 'A' && ch <= 'Z')
+            if (char.IsLetter((char)e.KeyValue) && isCapsLock())
             {
-                ch = (char)(ch + 32);
-                input += ch;
-
-
-
-                if (input[iWritingTextIndex] == text[iRealTextIndex])
-                {
-                    typing_Board.set_color((short)(iWritingTextIndex), colCorrectType);
-                }
-                else
-                {
-                    typing_Board.set_color((short)(iWritingTextIndex), colWrongType);
-
-                }
-
-                iRealTextIndex++;
-                iWritingTextIndex++;
+                add_new_Char((char)(e.KeyValue));
             }
-            else if(ch >= '1' && ch <= '9')
+            if (char.IsLetter((char)e.KeyValue) && !isCapsLock())
             {
-                input += ch;
+                add_new_Char((char)(e.KeyValue + 32));
+            }
+            else if(char.IsDigit((char)e.KeyValue))
+            {
+
+                add_new_Char((char)e.KeyValue);
 
 
-                if (input[iWritingTextIndex] == text[iRealTextIndex])
+            }
+            else if(char.IsPunctuation((char)e.KeyValue) )
+            {
+                add_new_Char((char)e.KeyValue);
+
+            }
+            else if (e.KeyValue == 190)
+            {
+                add_new_Char('.');
+            }
+            void add_new_Char(char ch1)
+            {
+                input += ch1;
+                current_word += ch1;
+
+
+                if (current_word.Length <= words[iCurrent_word_index].Length)
                 {
+                    if (current_word[current_word.Length - 1] == words[iCurrent_word_index][current_word.Length - 1])
+                    {
 
-                    typing_Board.set_color((short)(input.Length - 1), colCorrectType);
+                        typing_Board.set_color((short)(input.Length - 1), colCorrectType);
 
+                    }
+                    else
+                    {
+                        typing_Board.set_color((short)(input.Length - 1), colWrongType);
+
+                    }
+                    iRealTextIndex++;
+                    iWritingTextIndex++;
+
+                    return;
                 }
-                else
-                {
-                    typing_Board.set_color((short)(input.Length - 1), colWrongType);
 
-                }
-
-                iRealTextIndex++;
-                iWritingTextIndex++;
             }
 
         }
-
         private void handle_speachal_input(KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Back)
+            if (e.KeyCode == Keys.Back)
             {
-                if (input != "")
+                if (current_word != "")
                 {
+
 
                     typing_Board.set_color((short)(input.Length - 1), colNoType);
                     input = input.Remove(input.Length - 1, 1);
+                    current_word = current_word.Remove(current_word.Length - 1, 1);
 
-                    if(iRealTextIndex>0)
-                    iRealTextIndex   --;
+                    if (iRealTextIndex > 0)
+                        iRealTextIndex--;
 
-                    if(iWritingTextIndex>0)
-                    iWritingTextIndex--;
+                    if (iWritingTextIndex > 0)
+                        iWritingTextIndex--;
+
+                 
+                }
+                else if (iCurrent_word_index != 0)
+                {
+                    current_word = writed_words.Pop() + " ";
+
+                    //for(int i = input.Length - 1;i>= 0; i--)
+                    //{
+                    //    if (input[i] != ' ')
+                    //    {
+                    //        input.Remove(i);
+
+                    //    }
+                    //}
+
+                    int index = input.Length - 1;
+                    bool not_complit = false;
+                    while (input[index] == '\n' && index >= 0)
+                    {
+                        not_complit = true;
+                        input = input.Remove(index, 1);index--;
+                    }
+
+                    if (not_complit)
+                    {
+                        current_word = current_word.Remove(current_word.Length - 1, 1);
+
+                    }
+
+                    if (iRealTextIndex > 0)
+                        iRealTextIndex--;
+
+                    if (iWritingTextIndex > 0)
+                        iWritingTextIndex--;
+
+                    if (iCurrent_word_index > 0)
+
+                        iCurrent_word_index--;
                 }
 
             }
-            else if(e.KeyCode == Keys.Space)
+            else if (e.KeyCode == Keys.Space)
             {
-                input += " ";
-                iRealTextIndex++;
-                iWritingTextIndex++;
+                if (current_word.Length >= words[iCurrent_word_index].Length - 1)
+                {
+                    input += " ";
+                    iRealTextIndex++;
+                    iWritingTextIndex++;
+                    iCurrent_word_index++;
+                    writed_words.Push(current_word);
+                    current_word = "";
+
+
+                }
+                else
+                {
+                    input += get_spaces(words[iCurrent_word_index].Length-current_word.Length );
+
+                    iRealTextIndex = 0;
+
+                    for(int i = 0; i <= iCurrent_word_index; i++)
+                    {
+                        iRealTextIndex += words[iCurrent_word_index].Length;
+                    }
+                    iRealTextIndex--;
+                    iWritingTextIndex += words[iCurrent_word_index].Length - current_word.Length + 1;
+                    writed_words.Push(current_word);
+                    current_word = "";
+                    iCurrent_word_index++;
+
+                }
+
+
+
+
             }
-            
+
+            string get_spaces(int count)
+            {
+                string spaces = "";
+                for(int i = 0; i < count; i++)
+                {
+                    spaces+= "\n";
+                }
+
+                return spaces;
+            }
+
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        public static extern short GetKeyState(int keyCode);
+
+        private bool isCapsLock()
+        {
+            return (((ushort)GetKeyState(0x14)) & 0xffff) != 0;
+
         }
 
         private void ucTypingBoard_Load(object sender, EventArgs e)
